cc3498396f7186db26732d44898aa4d5c0aa473a
XiaoSoar <105652758+xiaosoar@users.noreply.github.com>
2025-06-01 13:24:36 +0800

    Update Footer.tsx
    删除部分链接

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -25,37 +25,8 @@ const Footer: React.FC = () => {
               {t("common.terms", "使用条款")}
             </Link>
             <Link to="/about" className="hover:text-primary transition-colors">
-              {t("common.about", "关于我们")}
+              {t("common.about", "关于本站")}
             </Link>
-          </div>
-          <div className="flex justify-center items-center space-x-4">
-            <a
-              href="https://zaunist.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-primary transition-colors"
-            >
-              <i className="fas fa-blog mr-1"></i>
-              {t("common.blog")}
-            </a>
-            <a
-              href="https://www.youtube.com/@zaunist"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-primary transition-colors"
-            >
-              <i className="fab fa-youtube mr-1"></i>
-              {t("common.youtube")}
-            </a>
-            <a
-              href="https://xugou.mdzz.uk"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-primary transition-colors"
-            >
-              <i className="fas fa-chart-line mr-1"></i>
-              {t("common.xugouMonitor")}
-            </a>
           </div>
         </div>
       </Container>
